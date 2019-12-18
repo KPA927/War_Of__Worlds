18186ba6f8b61c4e4761f1093cfcfdf66387cf04
KPA927 <kamenskikh.pasha@mail.ru>
2019-12-19 01:58:58 +0300

    Add IP read

--- server.py
+++ server.py
@@ -20,6 +20,14 @@ serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
 serv_sock.bind(('', 8007))
 serv_sock.listen(10)
 
+IP = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+        if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)),
+                                            s.getsockname()[0],
+                                            s.close()) for s in [socket.socket(socket.AF_INET,
+                                                                               socket.SOCK_DGRAM)]][0][1]]) +
+      ["no IP found"])[0]
+print('IP сервера:', IP)
+print('Пожалуйста, введите IP сервера в программы клиентов.')
 
 def _from_rgb(rgb):
     return "#%02x%02x%02x" % rgb
